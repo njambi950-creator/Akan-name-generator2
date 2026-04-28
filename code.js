b48c3953357947bb92692c551578cb2c49fc7e3e
Njambi <njambingugi950@gmail.com>
2026-04-28 11:04:13 +0300

    Fix typos in getElementById and retrieve user input values in form submission

--- code.js
+++ code.js
@@ -4,3 +4,10 @@ document
     // Prevent the form from refreshing the page
     event.preventDefault();
 
+// 1. Retrieve users input (Fixed typos in getElementById)
+    const year = document.getElementById("year").value;
+    const month = document.getElementById("month").value;
+    const day = document.getElementById("day").value;
+    const gender = document.querySelector(
+      'input[name="gender"]:checked',
+    )?.value;
